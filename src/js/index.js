3155e95594e0cc4668588405a24732654d144a73
Leo Bruell <leobruell@gmail.com>
2023-01-10 11:00:35 -0500

    Make basic add task functions work

--- src/js/index.js
+++ src/js/index.js
@@ -25,26 +25,49 @@ class MyToDoItem {
 const myToDoList = [];
 
 function addTask(event) {
-  const theTitle = document.querySelector(".title input").value;
-  const theDescription = document.querySelector(".description input").value;
-  const theDate = document.querySelector(".date input").value;
+  const theTitle = document.querySelector("#title").value;
+  const theDescription = document.querySelector("#Description").value;
+  const theDate = document.querySelector("#Date").value;
   const newTask = new MyToDoItem(theTitle, theDescription, theDate);
-  myToDoList.push(newTask);
-  console.log(myToDoList);
-  displayAllTasks();
+  if (theTitle != "" && theDescription != "" && theDate != "") {
+    myToDoList.push(newTask);
+    displayAllTasks();
+    const addTaskForm = document.querySelector("#add-class-form");
+    addTaskForm.reset();
+    addTaskForm.classList.add("d-none");
+  }
 }
 
 function displayAllTasks() {
   const allTasksList = document.querySelector(".all-tasks-list");
+  allTasksList.innerHTML = "";
   myToDoList.forEach((task) => {
     const aTask = document.createElement("div");
-    aTask.classList.add("a-task");
-    aTask.innerHTML = `<h3 class="task-title">${task.title}</h3>
+    aTask.innerHTML = `<div class="border border-dark d-flex flex-row justify-content-between ps-4 pe-5 pt-2 mb-3">
+    <h3 class="task-title">${task.title}</h3>
     <p class="task-description">${task.description}</p>
-    <p class="task-due">${task.dueDate}</p>`;
+    <div class="date-div">
+      <p class="border border-dark p-1">${task.dueDate}</p>
+    </div>
+  </div>`;
     allTasksList.appendChild(aTask);
   });
 }
 
-const addTaskButton = document.querySelector(".submit-task-form-button");
-addTaskButton.addEventListener("click", addTask);
+function openTaskForm() {
+  const addTaskForm = document.querySelector("#add-class-form");
+  addTaskForm.classList.remove("d-none");
+  const addTaskButton = document.querySelector(".add-task-button");
+  const cancelTaskButton = document.querySelector(".cancel-task-button");
+  cancelTaskButton.addEventListener("click", cancelTaskForm);
+  addTaskButton.addEventListener("click", addTask);
+}
+
+function cancelTaskForm() {
+  const addTaskForm = document.querySelector("#add-class-form");
+  addTaskForm.reset();
+  addTaskForm.classList.add("d-none");
+}
+
+const openTaskButton = document.querySelector(".open-task-form-button");
+openTaskButton.addEventListener("click", openTaskForm);
